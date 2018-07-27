a206314304453b637d5b4169067ef8b45d42a427
Daniil Fedotov <hairyhum@gmail.com>
2018-07-27 13:28:43 +0100

    graple

--- Assets/Scipts/OwlCat.cs
+++ Assets/Scipts/OwlCat.cs
@@ -12,26 +12,28 @@ public class OwlCat : MonoBehaviour {
     float jumpSpeed = 20f;
     float runSpeed = 10f;
 
-    bool grounded = false;
+    public bool grounded = false;
+    public bool grapled = false;
+
     float groundCheckRadius = 0.2f;
+    float grapleCheckRadius = 0.5f;
 
     public LayerMask ground;
+    public LayerMask wood;
     public Transform groundCheck;
+    public Transform grapleCheck;
 
     // Use this for initialization
 	void Start () {
 		
 	}
 
-	private void FixedUpdate()
+	void FixedUpdate()
 	{
-        var foo = 1;
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
-        if(grounded){
-            GetComponent<SpriteRenderer>().color = Color.white;
-        } else {
-            GetComponent<SpriteRenderer>().color = Color.grey;
-        }
+        CheckGround();
+        CheckGraple();
+        UpdateColor();
+
         var control = Input.GetAxis("Horizontal");
 
         speed = Math.Abs(control * runSpeed);
@@ -41,25 +43,58 @@ public class OwlCat : MonoBehaviour {
         Turn(control);
 	}
 
+    void CheckGround() {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ground);
+    }
+
+    void CheckGraple() {
+        grapled = !grounded && Physics2D.OverlapCircle(grapleCheck.position, grapleCheckRadius, wood);
+    }
+
+    void UpdateColor(){
+        if (grounded)
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        } else if (grapled) {
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        } else {
+            GetComponent<SpriteRenderer>().color = Color.grey;
+        }
+    }
+
     void Turn(float control) {
         var spriteRenderer = GetComponent<SpriteRenderer>();
 
         if (control < 0)
         {
-            direction = Direction.Left;
-            spriteRenderer.flipX = true;
+            if (direction == Direction.Right)
+            {
+                direction = Direction.Left;
+                FlipDirection();
+            }
         }
         else if (control > 0)
         {
-            direction = Direction.Right;
-            spriteRenderer.flipX = false;
+            if (direction == Direction.Left)
+            {
+                direction = Direction.Right;
+                FlipDirection();
+            }
         }
+    }
+
+    void FlipDirection(){
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    bool CanJump() {
+        return grounded || grapled;
     }
 
 	// Update is called once per frame
 	void Update () {
         var jumpButton = Input.GetKeyDown(KeyCode.Space);
-        if(jumpButton && grounded){
+        if(jumpButton && CanJump()){
             var rb = GetComponent<Rigidbody2D>();
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
